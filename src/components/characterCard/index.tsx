d5a27ebbb37d2166061a91a06cca46852041f51a
João Victor <jvbrandao98@gmail.com>
2023-01-09 13:32:22 -0300

    added new characters to character player

--- src/components/characterCard/index.tsx
+++ src/components/characterCard/index.tsx
@@ -1,4 +1,12 @@
-import { Card, CardContent, Typography, CardHeader } from "@mui/material";
+import React from "react";
+import {
+  Card,
+  CardContent,
+  Typography,
+  CardHeader,
+  CardActions,
+  Button,
+} from "@mui/material";
 import { Box } from "@mui/system";
 import styles from "./styles.module.scss";
 
@@ -8,6 +16,15 @@ interface Props {
   logo: string;
   characterClass: string;
   race: string;
+  level: number;
+  totalHP: number;
+  totalAC: number;
+  strength: number;
+  dexterity: number;
+  constitution: number;
+  intelligence: number;
+  wisdom: number;
+  charisma: number;
 }
 
 const CharacterCard = ({
@@ -16,25 +33,87 @@ const CharacterCard = ({
   logo,
   characterClass,
   race,
+  level,
+  totalHP,
+  totalAC,
+  strength,
+  dexterity,
+  constitution,
+  intelligence,
+  wisdom,
+  charisma,
 }: Props) => {
   return (
     <Card style={{ boxShadow: "2px 2px 1px #2f3e46", overflow: "auto" }}>
       <Box style={{ display: "flex", flexDirection: "row" }}>
         <img alt="" src={logo} className={styles.img} />
-        <CardHeader
-          title={characterName}
-          subheader={playerName}
-          className={styles.cardHeader}
-        />
+        <div>
+          <CardHeader
+            title={characterName}
+            subheader={playerName}
+            className={styles.cardHeader}
+          />
+          <div style={{ paddingLeft: "16px", paddingRight: "16px" }}>
+            <Typography sx={{ fontSize: 14 }} gutterBottom>
+              <b>Class:</b> {characterClass}
+            </Typography>
+            <Typography sx={{ fontSize: 14 }} gutterBottom>
+              <b>Race:</b> {race}
+            </Typography>
+            <Typography sx={{ fontSize: 14 }} gutterBottom>
+              <b>Level:</b> {level}
+            </Typography>
+            <Typography sx={{ fontSize: 14 }} gutterBottom>
+              <b>Total HP:</b> {totalHP}
+            </Typography>
+            <Typography sx={{ fontSize: 14 }} gutterBottom>
+              <b>Armor Class:</b>
+              {totalAC}
+            </Typography>
+          </div>
+        </div>
       </Box>
-      <CardContent>
-        <Typography sx={{ fontSize: 14 }} gutterBottom>
-          <b>Class:</b> {characterClass}
+      <CardContent
+        style={{
+          paddingBottom: "8px",
+          display: "flex",
+          flexDirection: "column",
+        }}
+      >
+        <Typography sx={{ fontSize: 14 }} style={{ fontWeight: "bold" }}>
+          Saving Throws:
         </Typography>
-        <Typography sx={{ fontSize: 14 }} gutterBottom>
-          <b>Race:</b> {race}
-        </Typography>
+        <div className={styles.saving_throws}>
+          <div>
+            <Typography sx={{ fontSize: 14 }} gutterBottom>
+              <b>Strength:</b> {strength}
+            </Typography>
+            <Typography sx={{ fontSize: 14 }} gutterBottom>
+              <b>Dexterity:</b> {dexterity}
+            </Typography>
+          </div>
+          <div>
+            <Typography sx={{ fontSize: 14 }} gutterBottom>
+              <b>Constitution:</b> {constitution}
+            </Typography>
+            <Typography sx={{ fontSize: 14 }} gutterBottom>
+              <b>Intelligence:</b> {intelligence}
+            </Typography>
+          </div>
+          <div>
+            <Typography sx={{ fontSize: 14 }} gutterBottom>
+              <b>Wisdom:</b> {wisdom}
+            </Typography>
+            <Typography sx={{ fontSize: 14 }} gutterBottom>
+              <b>Charisma:</b> {charisma}
+            </Typography>
+          </div>
+        </div>
       </CardContent>
+      <CardActions style={{ padding: "0px", justifyContent: "end" }}>
+        <Button>Edit</Button>
+        <Button>Delete</Button>
+      </CardActions>
     </Card>
   );
 };
--- src/features/players/index.tsx
+++ src/features/players/index.tsx
@@ -1,6 +1,8 @@
 import styles from "./styles.module.scss";
 import logo from "../../assets/Monk-Leonardo.jpg";
 import { CharacterCard } from "../../components/characterCard";
+import { Fab } from "@mui/material";
+import AddIcon from "@mui/icons-material/Add";
 
 const PlayersPage = () => {
   return (
@@ -11,49 +13,30 @@ const PlayersPage = () => {
         characterClass="Barbarian"
         race="Human"
         logo={logo}
+        level={3}
+        totalHP={28}
+        totalAC={17}
+        strength={2}
+        dexterity={2}
+        constitution={2}
+        intelligence={2}
+        wisdom={2}
+        charisma={2}
       />
-      <CharacterCard
-        playerName="Leonardo"
-        characterName="Edinho Chacal"
-        characterClass="Barbarian"
-        race="Human"
-        logo={logo}
-      />
-      <CharacterCard
-        playerName="Leonardo"
-        characterName="Edinho Chacal"
-        characterClass="Barbarian"
-        race="Human"
-        logo={logo}
-      />
-      <CharacterCard
-        playerName="Leonardo"
-        characterName="Edinho Chacal"
-        characterClass="Barbarian"
-        race="Human"
-        logo={logo}
-      />
-      <CharacterCard
-        playerName="Leonardo"
-        characterName="Edinho Chacal"
-        characterClass="Barbarian"
-        race="Human"
-        logo={logo}
-      />
-      <CharacterCard
-        playerName="Leonardo"
-        characterName="Edinho Chacal"
-        characterClass="Barbarian"
-        race="Human"
-        logo={logo}
-      />
-      <CharacterCard
-        playerName="Leonardo"
-        characterName="Edinho Chacal"
-        characterClass="Barbarian"
-        race="Human"
-        logo={logo}
-      />
+      <Fab
+        color="primary"
+        aria-label="add"
+        style={{
+          background: "#433220",
+          color: "white",
+          position: "fixed",
+          right: "2%",
+          zIndex: "1300",
+          top: "22px",
+        }}
+      >
+        <AddIcon />
+      </Fab>
     </div>
   );
 };
